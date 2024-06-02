a2fc917fe831416cb78d77200f1a565434710a11
Jyotirmay Biswas <132752040+Jyotirmay-ai@users.noreply.github.com>
2024-06-02 07:33:13 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -41,29 +41,6 @@ if (exampleModal) {
 
 
 
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
-
-const submitSuccess = document.querySelector('.submit-success')
-const submitError = document.querySelector('.submit-error')
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let myForm = document.getElementById("ajaxForm");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    
-    .then(() => { submitSuccess.style.display = 'block'; submitError.style.display = null; })
-    .catch((error) => { submitError.style.display = 'block'; submitSuccess.style.display = null; });
-};
-
-
-
 
 
 
